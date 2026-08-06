539d45bc775661ed649cfe3082dc0d98fa03aee3
valenbalzaa <valenbalzalantean@gmail.com>
2026-08-06 09:44:19 -0300

    Crear clubSeason.ts

---
+++ src/types/clubSeason.ts
@@ -0,0 +1,29 @@
+export type ClubSeason = {
+
+  clubId: number;
+
+  points: number;
+
+  played: number;
+
+  wins: number;
+
+  draws: number;
+
+  losses: number;
+
+  goalsFor: number;
+
+  goalsAgainst: number;
+
+  goalDifference: number;
+
+  position: number;
+
+  money: number;
+
+  morale: number;
+
+  reputation: number;
+
+};
