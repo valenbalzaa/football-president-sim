0c32b6966d735e2096d16ac7e0eba8353aa1f01c
valenbalzaa <valenbalzalantean@gmail.com>
2026-08-06 08:30:21 -0300

    Crear standing.ts

---
+++ src/data/standing.ts
@@ -0,0 +1,19 @@
+export type Standing = {
+  club: string;
+
+  played: number;
+
+  wins: number;
+
+  draws: number;
+
+  losses: number;
+
+  goalsFor: number;
+
+  goalsAgainst: number;
+
+  goalDifference: number;
+
+  points: number;
+};
